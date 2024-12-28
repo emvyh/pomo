5973c615f5def1417b4c7e124bcfea57f7693331
Emily Hoang <emilyhoanggs@gmail.com>
2024-12-28 00:42:19 -0500

    everything except timer

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function popup(){
 
+function popup() {
+    const pop = document.getElementById("pop");
+    const head = document.getElementById("poplink");
+
+    head.addEventListener("click", function() {   
+            pop.style.display = "block"; // Show the popup
+    });
+    document.addEventListener("click", function(event) {
+        if (!pop.contains(event.target) && event.target !== head) {
+            pop.style.display = "none"; // Hide the popup if click is outside the popup
+        }
+    });
 }
